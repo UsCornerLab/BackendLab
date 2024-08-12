fcb8f2a1b0b842ef88c265fc0789b94365b79aa2
Makbelhailu <mcwelson92@gmail.com>
2024-08-12 14:22:02 +0200

    🐞 fix(migration, .env and controller): made the register route to accept file(id photo) and store it on the storage(local) and insert the link into the database made the .env url pushable to all y'all use the same env lastly fixing the error response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\Storage;
 class AuthController extends Controller
 {
     public function register(Request $request) {
@@ -21,28 +21,40 @@ class AuthController extends Controller
                 'password' => 'required|string|min:8',
                 'age' => 'required|integer',
                 "address" => 'required|string|max:255',
-                "id_photo_path" => "required|string",
+                "id_photo" => "required|file|max:10240",
             ]);
+
+            if($request->hasFile('id_photo')) {
+                $file = $request->file('id_photo');
+                $fileName = time().'_'.$file->getClientOriginalName();
+                $filePath = $file->storeAs('ID_photos', $fileName,'public');
+
+                $url = Storage::url($filePath);
+
+                $user = User::create([
+                    'first_name' => $data['firstName'],
+                    'last_name' => $data['lastName'],
+                    'email' => $data['email'],
+                    'password' => bcrypt($data['password']),
+                    'age' => $data['age'],
+                    'address' => $data['address'],
+                    'id_photo_path' => $url,
+                ]);
+            } else {
+                return response()->json(['status'=> false,'message' => "ID photo required"], 500);
+            }
     
-            $user = User::create([
-                'first_name' => $data['firstName'],
-                'last_name' => $data['lastName'],
-                'email' => $data['email'],
-                'password' => bcrypt($data['password']),
-                'age' => $data['age'],
-                'address' => $data['address'],
-                'id_photo_path' => $data['id_photo_path'],
-            ]);
 
             
     
             return response()->json([
+                'status'=> true,
                 'message' => 'Registered successfully',
             ], 201);
 
         } catch (Exception $e) {
             Log::error('An error occurred: ' . $e->getMessage());
-            return response()->json(['error' => $e->getMessage()], 500);
+            return response()->json(['status'=> false,'message' => $e->getMessage()], 500);
         }
     }
 
@@ -55,6 +67,7 @@ class AuthController extends Controller
 
             if(!$token = auth()->attempt($data)) {
                 return response([
+                    'status'=> false,
                     'message' => "Email or Password are wrong"
                 ]);
             }
@@ -63,24 +76,26 @@ class AuthController extends Controller
             // $token = $user->createToken("access_token", expiresAt:now()->addDay())->plainTextToken;
 
             return response()->json([
+                'status'=> true,
                 "message"=> "Logged In successfully",
                 "user" => $user,
                 "token" => $token
             ]);
         } catch (Exception $e) {
             Log::error('An error occurred: ' . $e->getMessage());
-            return response()->json(['error' => $e->getMessage()], 500);
+            return response()->json(['status'=> false,'message' => $e->getMessage()], 500);
+
         }
     }
 
     public function logout(Request $request) {
         try{
             auth()->logout(true);
-            return response()->json(['message' => "Logged out successfully"], 200);
+            return response()->json(['status'=> true, 'message' => "Logged out successfully"], 200);
            
         } catch (Exception $e) {
             Log::error('An error occurred: ' . $e->getMessage());
-            return response()->json(['error' => $e->getMessage()], 500);
+            return response()->json(['status'=> false,'message' => $e->getMessage()], 500);
         }
     
     }
