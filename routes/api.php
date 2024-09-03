cbd1e7d58f6af8a432a4f489bac5832aed4b5ed7
Hayleyesus Tekalign <hahube22to21@gmail.com>
2024-09-03 12:54:07 +0300

    🌟 feat: Enhance book request feature with CRUD operations
    Added separate handling for authenticated and unauthenticated users in the BookRequestController.
    Implemented validation and proper status handling for book requests.
    This enhancement aligns with the SRS requirements for the book request feature.

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-
+use App\Http\Controllers\BookRequestController;
+use App\Http\Controllers\FileController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -15,26 +17,22 @@ Route::middleware(['access'])->group(function () {
     Route::get('/user', [AuthController::class, 'getUser']);
     Route::put('/updateProfile/{id}', [AuthController::class, 'updateProfile']);
 
+    
     Route::get('/books', [BookController::class,'getAll']);
     Route::get('/books/{id}', [BookController::class,'getOne']);
-
     Route::middleware(['role:librarian,admin'])->group(function () {
         Route::post('/books', [BookController::class,'create']);
         Route::delete('/books/{id}', [BookController::class,'delete']);
         Route::put('/books/{id}', [BookController::class,'update']);
     });
-
     Route::get('/books/search', [BookController::class, 'search']);
 
-    Route::middleware(['role:librarian,admin'])->get('/files/{fileName}', function ($fileName) {
-        // You can add additional checks here if necessary
-    
-        if (Storage::disk('public')->exists("ID_photos/$fileName")) {
-            return response()->download(storage_path("app/public/ID_photos/$fileName"));
-        }
-    
-        return abort(404, 'File not found');
-    });
+    Route::post('/book-requests', [BookRequestController::class, 'store']); 
+    Route::get('/book-requests/{id}', [BookRequestController::class, 'show']);
+    Route::put('/book-requests/{id}', [BookRequestController::class, 'update']);
+    Route::delete('/book-requests/{id}', [BookRequestController::class, 'destroy']);
+
+    Route::middleware(['role:librarian,admin'])->get('/files/{fileName}', [FileController::class, 'serveFile']);
 });
 
 
