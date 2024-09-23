60f8bc9c32d9e75e54742bc7053c62b9be0ed920
Hayleyesus Tekalign <hahube22to21@gmail.com>
2024-09-24 01:09:50 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,44 +2,45 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use Illuminate\Http\Request;
+use Laravel\Sanctum\Sanctum;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\BookSupportRequestController;
 use App\Http\Controllers\SupportedBooksController;
 use App\Http\Controllers\FileController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('access');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
-Route::middleware(['access'])->group(function () {
-    Route::get('/user', [AuthController::class, 'getUser']);
-    Route::put('/updateProfile/{id}', [AuthController::class, 'updateProfile']);
 
-    Route::middleware(['role:librarian,admin'])->group(function () {
-        Route::get('/users', [AuthController::class, 'getUsers']);
-        Route::put('/verifyUser/{id}', [AuthController::class, 'verifyUser']);
-    });
+Route::middleware(['auth'])->group(function () {
 
-    Route::get('/books', [BookController::class, 'getAll']);
-    Route::get('/books/{id}', [BookController::class, 'getOne']);
+    
+    Route::get('/books', [BookController::class,'getAll']);
+    Route::get('/books/{id}', [BookController::class,'getOne']);
+    Route::post('/books', [BookController::class,'create']);
+    Route::delete('/books/{id}', [BookController::class,'delete']);
+    Route::put('/books/{id}', [BookController::class,'update']);
 
-    Route::middleware(['role:librarian,admin'])->group(function () {
-        Route::post('/books', [BookController::class, 'create']);
-        Route::delete('/books/{id}', [BookController::class, 'delete']);
-        Route::put('/books/{id}', [BookController::class, 'update']);
-        Route::put('/books/activate/{id}', [BookController::class, 'activate']);
-        Route::put('/books/deactivate/{id}', [BookController::class, 'deactivate']);
-    });
+    Route::get('/files/{fileName}', [FileController::class, 'serveFile']);
+});
 
-    Route::get('/books/search', [BookController::class, 'search']);
 
-    Route::middleware(['auth:api'])->group(function () {
-        Route::post('/book-support-requests', [BookSupportRequestController::class, 'store']);
-        Route::put('/book-support-requests/{id}/review', [BookSupportRequestController::class, 'review']);
-        
-        Route::post('/supported-books', [SupportedBooksController::class, 'store']);
-        Route::get('/supported-books', [SupportedBooksController::class, 'index']);
-        Route::get('/supported-books/{id}', [SupportedBooksController::class, 'show']);
-        Route::put('/supported-books/{id}', [SupportedBooksController::class, 'update']);
-        Route::delete('/supported-books/{id}', [SupportedBooksController::class, 'destroy']);
-    });
+Route::get('/book/search', [BookController::class, 'search']);
+
+Route::get('/files/{fileName}', function ($fileName) {
+    if (Storage::disk('public')->exists('ID_photos/' . $fileName)) {
+        return response()->download(storage_path('app/public/ID_photos/' . $fileName));
+    }
+    return abort(404, 'File not found');
+});
+Route::middleware(['auth:api'])->group(function () {
+    Route::post('/book-support-requests', [BookSupportRequestController::class, 'store']);
+    Route::put('/book-support-requests/{id}/review', [BookSupportRequestController::class, 'review']);
+    Route::post('/supported-books', [SupportedBooksController::class, 'store']);
+    Route::get('/supported-books', [SupportedBooksController::class, 'index']);
+    Route::get('/supported-books/{id}', [SupportedBooksController::class, 'show']);
+    Route::put('/supported-books/{id}', [SupportedBooksController::class, 'update']);
+    Route::delete('/supported-books/{id}', [SupportedBooksController::class, 'destroy']);
 });
